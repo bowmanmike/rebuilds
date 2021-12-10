4026ccc7e31b62a40f3da9df6123e425fdc162ae
Mike Bowman <mbowman2@gmail.com>
2021-12-10 14:19:09 -0500

    Upgrade to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {
-      colors: {
-        orange: colors.orange,
-        amber: colors.amber,
-        coolGray: colors.coolGray,
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
